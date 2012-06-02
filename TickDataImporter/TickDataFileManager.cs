b2b63e9a13bd7f7492e0e76c85c77177197929c8
Wilhelm Fritsche <wilhelm.fritsche@gmail.com>
2012-06-02 09:04:53 +0200

    added Filemanager

--- TickDataImporter/TickDataFileManager.cs
+++ TickDataImporter/TickDataFileManager.cs
@@ -1,11 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
+using Ionic.Zip;
 
 namespace TickDataImporter
 {
-    class TickDataFileManager
+    internal class TickDataFileManager
     {
+        private TickDataFileManager() { }
+
+        public TickDataFileManager(string pathDirectoryTickData, string pathTemporaryOutputfolder)
+        {
+        }
+
+        private void ExtractZip(string file, string extractToFolder)
+        {
+            string zipToUnpack = file;
+            string unpackDirectory = extractToFolder;
+            using (ZipFile zip1 = ZipFile.Read(zipToUnpack))
+            {
+                // here, we extract every entry, but we could extract conditionally
+                // based on entry name, size, date, checkbox status, etc.
+                foreach (ZipEntry e in zip1)
+                {
+                    e.Extract(unpackDirectory, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+        }
+
+        private static void ExtractGz(string filePath, string toFolder)
+        {
+            var fi = new FileInfo(filePath);
+            // Get the stream of the source file.
+            using (FileStream inFile = fi.OpenRead())
+            {
+                // Get original file extension, for example
+                // "doc" from report.doc.gz.
+                string curFile = fi.FullName;
+
+                //Create the decompressed file.
+                using (FileStream outFile = File.Create(Path.Combine(toFolder, Path.GetFileNameWithoutExtension(filePath))))
+                {
+                    using (GZipStream Decompress = new GZipStream(inFile,
+                            CompressionMode.Decompress))
+                    {
+                        // Copy the decompression stream
+                        // into the output file.
+                        Decompress.CopyTo(outFile);
+                    }
+                }
+            }
     }
 }
